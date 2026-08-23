1f1fe50192580cb9e8f9bda33285ecb4faeefdd2
Jimi Spier <jimi.aj7gw@gmail.com>
2026-08-23 16:10:51 -0700

    folders and stuff

---
+++ c_std/test/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//#include <stdlib.h>
+
+
+int main() {
+  int x, y, z;
+  //declaring several vars of the same type all at once.
+  x = y = z = 50;
+  //assigning all the variables the same number.
+  printf("%d", x + y + z);
+  return 0;
+}
+
+
+
