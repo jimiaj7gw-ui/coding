fbf10d10ce5bd36ddea0dca1c34a36fecbf862d3
Jimi Spier <jimi.aj7gw@gmail.com>
2026-08-23 17:43:06 -0700

    saving

---
+++ cpp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x, y, z;
+    
+    // Assigning all variables the same number
+    x = y = z = 50;
+    
+    // Printing the sum
+    cout << x + y + z << endl;
+    
+    return 0;
+}
