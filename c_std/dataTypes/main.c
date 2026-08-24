ee43793c5983bf6dfbd242f166287a4fd6215a4e
Jimi Spier <jimi.aj7gw@gmail.com>
2026-08-23 20:49:03 -0700

    relearning everything

---
+++ c_std/dataTypes/main.c
@@ -0,0 +1,29 @@
+/******************/ 
+/******************/ 
+// **Jimi Spier** // 
+/*****************/ 
+/*****************/ 
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    int num;//declared but unused variable
+    int age = 40; 
+
+    double gpa =40.0; //Doubles hold decimals
+    
+    char grade = 'A'; //Hold single char
+    char name[] = "Do"; //an Array that will hold multiple chars
+    
+    int* ptr = &age; // Points to a location in memory where age is. Like a hyperlink to a resource elsewhere in the code/memory
+    
+    printf("%d \n",age); //uses int age using %d for Decimal//
+    printf("%.2f\n",gpa); // uses double gpa with .2 places of precision, with %f for floating decimal. 
+    printf("%c\n",grade); // uses single char grade with the %c for single Character.
+    printf("%s\n",name); // takes array of char name[] with %s for String for characters. 
+    printf("Pointer to age's memory location in Hex: %p \n",ptr); // uses int* ptr to indicate ptr is pointing to &age address location with %p for pointer.
+    printf("Dereferenced back to age (original value): %d \n",*ptr); // Dereferencing the ptr with * returns the value that was in the memory location (original value).
+    printf("Size of name plus one... %zu\n", sizeof(name)); // returns the size of the string +1. 
+    
+    return 0;
+}
