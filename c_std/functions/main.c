0323dc615b39eb821b93fe072cac0a6a6514a860
Jimi Spier <jimi.aj7gw@gmail.com>
2026-08-24 01:08:27 -0700

    more updated notes

--- c_std/functions/main.c
+++ c_std/functions/main.c
@@ -13,6 +13,8 @@
 #include <stdio.h>
 
 void funcONE(char fName[],char lName[], int mAge ){ // vars: nameFirst, nameLast, & age get passed here for processing
+//using void because this function prints directly to the console and doesn't send anything back to main.
+
 
 printf("Your name is: %s %s and you are %d years old.\n", fName, lName, mAge);  
 //Variables will display in order as written. Specifiers have to match
@@ -20,13 +22,15 @@ printf("Your name is: %s %s and you are %d years old.\n", fName, lName, mAge);
 
 };
 
+//*****************Variables********************/
+char nameFirst[10]; // char array with 10 spaces
+char nameLast[10]; // char array with 10 spaces
+int age = 0; // whole number var with 0 initialization to dump trash data.
 
+
+//**************Program Start*******************/
 int main() {
 
-//*****************Variables********************/
-char nameFirst[10];
-char nameLast[10];
-int age = 0;
 
 
 
@@ -51,5 +55,5 @@ scanf("%d", &age);//The '&' tells the compiler, Here is the physical memory addr
 funcONE(nameFirst, nameLast, age); //Pass into funcONE the three user inputs.
 
     
-    return 0;
+    return 0;//exit normally
 }
