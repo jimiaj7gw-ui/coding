fbb8825b2a8429909451bb81b3d3837a1d4450b9
Jimi Spier <jimi.aj7gw@gmail.com>
2026-08-23 18:08:16 -0700

    testing stuff out

--- python/test/test.py
+++ python/test/test.py
@@ -1,6 +1,5 @@
-def main():
     # Declaring and assigning all variables the same value at once
-    x = y = z = 50
+x = y = z = 50
     
     # Adding them together and printing the result
-    print(x + y + z)
+print(x + y + z)
