8ae3fed8e42f78514ba96dd3f06296b40f240053
Jimi Spier <jimi.aj7gw@gmail.com>
2026-08-28 19:01:20 -0700

    new program

---
+++ c_std/calculator/calculator.c
@@ -0,0 +1,87 @@
+/**********************************************/
+/**********************************************/
+//********Name: Jimi Spier*********************/
+//********Callsign: AJ7GW *********************/
+/**********************************************/
+/**********************************************/
+//******Program Name: Console Calculator*******/
+/**********************************************/
+/**********************************************/
+
+
+//******************Headers********************//
+#include <stdio.h>
+#include <stdlib.h>
+
+int userChoice;
+
+int firstNumber = 0 ;
+int secondNumber = 0;
+int userAnswer=0;
+
+void userInstructions(){
+    printf("\n");
+    printf("\n");
+    printf("Please enter choice to begin: \n");    
+    printf("1 = + \n");
+    printf("2 = - \n");
+    printf("3 = * \n");
+    printf("0 = Exit\n");
+    printf("\n");
+};
+void userMath(int userChoice, int firstNumber, int secondNumber ){
+    
+    if (userChoice == 1){
+        userAnswer = firstNumber + secondNumber;
+        printf("\nYour answer is %d + %d = %d",firstNumber, secondNumber, userAnswer  );
+    }
+    else if (userChoice == 2) {
+        userAnswer = firstNumber - secondNumber;
+        printf("\nYour answer is %d - %d = %d",firstNumber, secondNumber, userAnswer  );
+    }
+    else if (userChoice ==3) {
+        userAnswer = firstNumber * secondNumber;
+        printf("\nYour answer is %d * %d = %d",firstNumber, secondNumber, userAnswer  );
+    }
+     
+           
+    
+
+
+
+
+};
+
+
+int main() {
+//*****************Variables********************/
+        
+    userInstructions();
+        printf("\n");
+        printf("\n");
+
+    printf("Enter your choice of operation: ");
+    scanf("%d",&userChoice);
+    if (userChoice ==0) {
+        printf("Thank you - AJ7GW ");
+        exit(0);}
+    else if (userChoice==1 || userChoice==2 || userChoice==3){   
+    printf("\n");
+    printf("Enter first number; ");
+    scanf("%d",&firstNumber);
+    printf("\n");
+    printf("Enter second number; ");
+    scanf("%d",&secondNumber);
+    printf("\n");
+    userMath(userChoice,  firstNumber,  secondNumber);}
+    else {
+        printf("Invalid input");}
+    
+
+
+//*******************Output*********************/
+
+
+    
+    return 0;
+}
